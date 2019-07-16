32e7893c2417d29c84cb2082f0fb1b9833212552
Samantha Hyatt <shhyatt@gmail.com>
2019-07-16 01:46:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function bfs(rootNode, vertices, edges){
 function findAdjacent(vertices, edges, nodeName) {
    let firstEdge = edges.filter(edge => {
     return edge.includes(nodeName);
-    console.log(firstEdge)
+    
   });
    
   
